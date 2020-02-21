34a21d342695b840a1f2198302894d1535f773de
Heny11110 <henoke.tadesse@gmail.com>
2020-02-21 16:45:19 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const app = express()
-app.set(path.join(__dirname,'/views'))
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-  res.send('root')
-})
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Index' })
-})
-const port = 3000
-app.listen(port, () => console.log('PS Project Running on port 3000! ${port}!'))
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+app.set('views',path.join(__dirname,'/views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')));
+ 
+app.get('/',   (req, res) =>  res.render('index', { title: 'Index' }));
+
+const port = 3000;
+app.listen(port, () => console.log('PS Project Running on port 3000! ${port}!'));
